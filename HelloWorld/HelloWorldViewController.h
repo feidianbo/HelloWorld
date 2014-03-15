6a6fd6f49f7ffa3c52acac6433fe3175c07f44ee
DEO <f.db@163.com>
2014-03-15 23:01:28 +0800

    创建项目
    创建演示项目

--- HelloWorld/HelloWorldViewController.h
+++ HelloWorld/HelloWorldViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface HelloWorldViewController : UIViewController
-
+@interface HelloWorldViewController : UIViewController <UITextFieldDelegate>
+@property (copy, nonatomic) NSString *userName;
 @end
--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -9,6 +9,9 @@
 #import "HelloWorldViewController.h"
 
 @interface HelloWorldViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+- (IBAction)changeGreeting:(id)sender;
 
 @end
 
@@ -26,4 +29,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    NSString *nameString = self.userName;
+    if ([nameString length] == 0){
+        nameString = @"World";
+    }
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString ];
+    self.label.text = greeting;
+}
+
+- (BOOL) textFieldShouldReturn: (UITextField *) theTextField{
+    if (theTextField == self.textField){
+        [theTextField resignFirstResponder];
+    }
+    
+    return YES;
+}
 @end
